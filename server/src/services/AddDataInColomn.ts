6c07bf2ca6e69819f35586411a6f2ae970cfc6e8
gwensneijders <125651877+gwensneijders@users.noreply.github.com>
2023-10-20 18:19:38 +0000

    functies addColumn en copyColumn gemaakt

--- server/src/services/AddDataInColomn.ts
+++ server/src/services/AddDataInColomn.ts
@@ -4,20 +4,26 @@ import ExcelJs from "exceljs";
 //AddDataToColumn(firstSheet);
 //http://localhost:3000/uploadfile/demo
 
-export const AddDataToColumn = (worksheet: ExcelJs.Worksheet)=>{
-    // De cellwaarde aanspreken, in vullen is = "-----" Het overschrijft de waarde. 
-        // console.log(worksheet.getCell("B1").value);
-
-        // console.log(wb);
-        
-        worksheet.spliceColumns(4,2, [], []);
-       
-
-    // table.removeColumns(1, 1);
-    // table.commit();
-
+export const AddColumn = (worksheet: ExcelJs.Worksheet, startColomn : number) :ExcelJs.Worksheet=>{
+    worksheet.spliceColumns(startColomn, 0, []);
+    worksheet = copyColumn(worksheet); 
     
+    return worksheet;     
+}; 
 
+const copyColumn = (worksheet: ExcelJs.Worksheet) : ExcelJs.Worksheet =>{
+    let keyToCopyToColumn: string = "L";
+    let keyCopyFromColumn: string = "K";  
+
+    const toCopytoColumn : ExcelJs.Column = worksheet.getColumn(keyToCopyToColumn);
+    toCopytoColumn.width = worksheet.getColumn(keyCopyFromColumn).width;
+
+    toCopytoColumn.eachCell(function(cell, rowNumber) {
+        cell.style = worksheet.getCell(keyCopyFromColumn +rowNumber).style;
+        cell.value = worksheet.getCell(keyCopyFromColumn +rowNumber).value;
+    });
+
+    return worksheet; 
 }
 
 
