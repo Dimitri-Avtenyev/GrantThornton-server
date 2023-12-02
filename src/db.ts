28247cbbfa016aca4ddc79feab1cd86934e113b7
Dimitri Avtenyev <dimitri.avtenyev@gmail.com>
2023-12-02 14:28:13 +0000

    add connect and close to db

--- src/db.ts
+++ src/db.ts
@@ -21,7 +21,23 @@ export const connectionString = (uri: string) => {
 connectionString(uri)
 export const dbClient = new MongoClient(connectionString(uri));
 
-
+const connectDb = async () => {
+  try {
+    await dbClient.connect();
+    console.log("Connection to db has been established.");
+    process.on("SIGINT", closeDb);
+  } catch (err) {
+    console.log("Conecting failed, error: " + err);
+  }
+}
+const closeDb = async () => {
+  try {
+    await dbClient.close();
+    console.log("Connection to the db has been closed.");
+  } catch (err) {
+    console.log("Closing connection failed, error:" + err);
+  }
+}
 
 // populate db 
 let populateddDb: boolean = false; //run only once on startup
